2859352ed0ca1dbe8620e75774d16d0d84b76ac8
Željko Romić <zrstrat92@gmail.com>
2025-06-24 18:10:27 +0200

    Implementacija pretraživanja liste internih fajlova

--- lib/features/internal_file/screens/internal_file_list_screen.dart
+++ lib/features/internal_file/screens/internal_file_list_screen.dart
@@ -10,19 +10,33 @@ class InternalFileListScreen extends StatefulWidget {
 }
 
 class _InternalFileListScreenState extends State<InternalFileListScreen> {
-  List<FileSystemEntity> internalFiles = [];
+  List<FileSystemEntity> allFiles = [];
+  List<FileSystemEntity> filteredFiles = [];
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _loadInternalFiles();
+    _searchController.addListener(_filterFiles);
   }
 
   Future<void> _loadInternalFiles() async {
     final dir = await getApplicationDocumentsDirectory();
     final files = dir.listSync().where((f) => f.path.endsWith('.txt')).toList();
     setState(() {
-      internalFiles = files;
+      allFiles = files;
+      filteredFiles = files;
+    });
+  }
+
+  void _filterFiles() {
+    final query = _searchController.text.toLowerCase();
+    setState(() {
+      filteredFiles = allFiles.where((file) {
+        final name = file.path.split('/').last.toLowerCase();
+        return name.contains(query);
+      }).toList();
     });
   }
 
@@ -31,19 +45,41 @@ class _InternalFileListScreenState extends State<InternalFileListScreen> {
   }
 
   @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Interni fajlovi')),
-      body: ListView.builder(
-        itemCount: internalFiles.length,
-        itemBuilder: (context, index) {
-          final file = internalFiles[index];
-          final fileName = file.path.split('/').last;
-          return ListTile(
-            title: Text(fileName),
-            onTap: () => _openFile(file.path),
-          );
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12),
+            child: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                labelText: 'Pretraži fajlove',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredFiles.length,
+              itemBuilder: (context, index) {
+                final file = filteredFiles[index];
+                final fileName = file.path.split('/').last;
+                return ListTile(
+                  title: Text(fileName),
+                  onTap: () => _openFile(file.path),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
